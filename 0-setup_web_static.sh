7cdf8e2f17b92e82a9ba74333955722806d95553
UkazPromise <ukazpromise@gmail.com>
2024-04-07 19:28:13 -0700

    0-setup_web_static.sh update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,28 +1,20 @@
 #!/usr/bin/env bash
-
-# Update package repository and install nginx
+# sets up web servers for the deployment of web_static.
 apt update -y
 apt install nginx -y
-
 ufw allow 'Nginx HTTP'
-
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-
-cat <<EOF > /data/web_static/releases/test/index.html
-<html>
+echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>
-EOF
-
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
-sed -i '/listen 80 default_server/a \
-        location /hbnb_static/ { \
-            alias /data/web_static/current/; \
-        }' /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
+
+
